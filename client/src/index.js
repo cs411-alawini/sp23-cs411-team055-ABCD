1e3777aac5a115d0c109160be816ccaa4367333b
Adipat2003 <99374679+Adipat2003@users.noreply.github.com>
2023-04-12 12:29:22 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home.js";
 import GraphPage from "./components/graph-page.js";
 import AreaReport from "./components/area-report.js";
+import ReportCrime from "./components/report-crime.js";
 import NotFound from "./components/not-found.js";
+
+import ProcessCrime from "./components/process-crime.js";
+import ProcessCrimes from "./components/process-crimes.js";
+import ADQ1 from "./components/adq1.js";
 
 // CSS
 import "./fonts.css";
@@ -16,9 +21,13 @@ let navblocks = [
   { link: "/", content: "Home", },
   { link: "/graph", content: "Graph", },
   { link: "/area-report", content: "Area Report", },
+  { link: "/report-crime", content: "Report Crime", },
 ];
 
-console.log("hello world");
+let adminNavblocks = [
+  { link: "/admin", content: "Home", },
+  { link: "/admin/process-crime", content: "Process User-report Crimes", },
+];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -28,6 +37,11 @@ root.render(
         <Route exact path="/" element={<Home navblocks={navblocks} />}></Route>
         <Route exact path="/graph" element={<GraphPage navblocks={navblocks} />}></Route>
         <Route exact path="/area-report" element={<AreaReport navblocks={navblocks} />}></Route>
+        <Route exact path="/report-crime" element={<ReportCrime navblocks={navblocks} />}></Route>
+        <Route exact path="/admin" element={<Home navblocks={adminNavblocks} />}></Route>
+        <Route exact path="/admin/process-crime" element={<ProcessCrimes navblocks={adminNavblocks} />}></Route>
+        <Route exact path="/admin/process-crime/:reportid" element={<ProcessCrime navblocks={adminNavblocks} />}></Route>
+        <Route exact path="/adq1" element={<ADQ1 navblocks={navblocks} />}></Route>
         <Route exact path="*" element={<NotFound navblocks={navblocks} />}></Route>
       </Routes>
     </Router>
